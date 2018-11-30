631c3a674c34b754bc181c8abf050c89cb9ee9ba
Roger Huba <rogerhuba@gmail.com>
2018-11-30 14:49:38 -0800

    cli help and request info changes

--- bitmap/transform.py
+++ bitmap/transform.py
@@ -1,9 +1,10 @@
 import cmd
 import sys
+import os
 
 
 class BitmapManipulator(cmd.Cmd):
-    intro = 'Welcome to the bitmap manipulator.   Type help or ? to list commands.\n'
+    intro = 'Welcome to the bitmap manipulator.  Type help or ? to list commands.\n'
     prompt = '> '
 
     @staticmethod
@@ -16,7 +17,7 @@ class BitmapManipulator(cmd.Cmd):
         Returns a line-item response of the file's header data.
         """
         # TODO: Complete these CLI methods for performing described actions
-        pass
+        #here we pass the file_in to get headers
 
     def do_transform(self, arg):
         """
@@ -26,19 +27,51 @@ class BitmapManipulator(cmd.Cmd):
 
         transform <original> <new> <transform>
 
-        Creates a new file at 'new' location with the provided 'transform' applied to the new file.
+        Creates a new file at 'new' location with the provided 'transform'
+        applied to the new file.
         """
         # TODO: Complete these CLI methods for performing described actions
-        pass
+        file_in = input('Enter file to transform:  ')
+        file_out = input('Enter name of the file to save to:  ')
+        # Need to add duplicate file checking
+        print('\n1) Change Color')
+        print('2) Change Size')
+        print('3) Rotate Picture')
+        print('4) Flip Picture \n')
+
+
+    def emptyline(self):
+        '''Handles when the user presses enter and submits an empty line'''
+        print('Type help or ? to list commands.\n')
+
+    def help_exit(self):
+        """ User enters help exit """
+        print('Type "exit" to leave the program')
+
+    def help_transform(self):
+        """ User enters help transform
+        transform input-file output-file
+        Will then prompt user for type of transformation to apply
+        """
+        print('Enter transform. Prompt for files and type of transformation')
+
+    def help_get_headers(self):
+        """ User enters help get_headers
+        After the user enters the file-in and file out will select the type
+        of transformation to take place on the file
+        """
+        print('Enter the command transform "file-in" "file-out"')
 
     @staticmethod
     def do_exit(args):
         """
-        To use this type in 'exit' and the CLI tool will cleanly exit the running application.
+        To use this type in 'exit' and the CLI tool will cleanly exit the
+        running application.
         """
+        print('Thank you for using the application')
         sys.exit()
 
 
 if __name__ == '__main__':
+    os.system('clear')
     BitmapManipulator().cmdloop()
-
