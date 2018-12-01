74bb9ba47c4896e22361986ceb9f2df672b9dbfa
tobyatgithub <toby.fangyuan@gmail.com>
2018-11-30 16:40:31 -0800

    added new transforms

--- bitmap/xformer.py
+++ bitmap/xformer.py
@@ -9,6 +9,7 @@ BITMAP_TRANSFORMED_FILE_NAME = os.path.join(SCRIPT_DIR, 'new_bmp.bmp')
 # print(Bitmap.get_headers(Bitmap.read_file(BITMAP_FILE_NAME)))
 
 bm = Bitmap.read_file(BITMAP_FILE_NAME)
-new_bm = Bitmap(bm.transform_bitmap_flip_horizontal())
+# new_bm = Bitmap(bm.transform_bitmap_flip_horizontal())
+new_bm = Bitmap(bm.transform_bitmap_whatif())
 new_bm.write_file(BITMAP_TRANSFORMED_FILE_NAME)
 # print(Bitmap.get_headers(Bitmap.read_file(BITMAP_TRANSFORMED_FILE_NAME)))
